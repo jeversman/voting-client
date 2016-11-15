cb46ca165a53fa925bbe1e00daa6bc4e309775aa
cypress <romka_gorev@mail.ru>
2016-11-15 11:41:15 +0600

    chapters 5.8, 5.9 completed

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 
 import reducer from './reducer';
 import App from './components/App';
@@ -10,15 +11,11 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        vote: {
-            pair: ['Sunshine', '28 Days Later'],
-            tally: {Sunshine: 2}
-        }
-    }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+    store.dispatch({type: 'SET_STATE', state})
+);
 
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer} />
@@ -31,6 +28,8 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('app')
 );
+
+
 
 
 
